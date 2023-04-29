a2f14ac71c501738db9760e0b8afebd3e5b3ab89
qazsato <qazsato@gmail.com>
2023-04-30 08:29:21 +0900

    fix test

--- test/index-test.js
+++ test/index-test.js
@@ -5,124 +5,95 @@ const tester = new TextLintTester();
 const rule = require("../src/index");
 // ruleName, rule, { valid, invalid }
 tester.run("rule", rule, {
-    valid: [
-        // no problem
-        {
-            text: "text"
-        },
-        // with rulePath
-        {
-            text: "text",
-            options: {
-                rulePath: "./test/custom.yml"
-            }
-        }
-    ],
-
-    invalid: [
+  invalid: [
         // single match
+    {
+      text: "して頂く",
+      output: "していただく",
+      errors: [
         {
-            text: "して頂く",
-            output: "していただく",
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"頂く\" => \"いただく\"",
-                    line: 1,
-                    column: 3
-                }
-            ]
-        },
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"頂く\" => \"いただく\"",
+          line: 1,
+          column: 3
+        }
+      ]
+    },
+    {
+      text: "して下さい",
+      output: "してください",
+      errors: [
         {
-            text: "して下さい",
-            output: "してください",
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"下さい\" => \"ください\"",
-                    line: 1,
-                    column: 3
-                }
-            ]
-        },
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"下さい\" => \"ください\"",
+          line: 1,
+          column: 3
+        }
+      ]
+    },
+    {
+      text: "して行く",
+      output: "していく",
+      errors: [
         {
-            text: "して行く",
-            output: "していく",
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"行く\" => \"いく\"",
-                    line: 1,
-                    column: 3
-                }
-            ]
-        },
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"行く\" => \"いく\"",
+          line: 1,
+          column: 3
+        }
+      ]
+    },
+    {
+      text: "して来て",
+      output: "してきて",
+      errors: [
         {
-            text: "して来て",
-            output: "してきて",
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"来\" => \"き\"",
-                    line: 1,
-                    column: 3
-                }
-            ]
-        },
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"来\" => \"き\"",
+          line: 1,
+          column: 3
+        }
+      ]
+    },
+    {
+      text: "お願い致します",
+      output: "お願いいたします",
+      errors: [
         {
-            text: "お願い致します",
-            output: "お願いいたします",
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"致し\" => \"いたし\"",
-                    line: 1,
-                    column: 4
-                }
-            ]
-        },
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"致し\" => \"いたし\"",
+          line: 1,
+          column: 4
+        }
+      ]
+    },
+    {
+      text: "泣き出す",
+      output: "泣きだす",
+      errors: [
         {
-            text: "泣き出す",
-            output: "泣きだす",
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"出す\" => \"だす\"",
-                    line: 1,
-                    column: 3
-                }
-            ]
-        },
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"出す\" => \"だす\"",
+          line: 1,
+          column: 3
+        }
+      ]
+    },
         // multiple match
-        {
-            text: `許して頂いた。
+    {
+      text: `許して頂いた。
 
 見て来よう。`,
-            output: `許していただいた。
+      output: `許していただいた。
 
 見てこよう。`,
-            errors: [
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"頂い\" => \"いただい\"",
-                    line: 1,
-                    column: 4
-                },
-                {
-                    message: "ひらがなで表記したほうが読みやすい補助動詞: \"来よ\" => \"こよ\"",
-                    line: 3,
-                    column: 3
-                }
-            ]
-        },
-        // with rulePath
+      errors: [
         {
-            text: "して頂く",
-            options: {
-                rulePath: "./test/custom.yml"
-            },
-            output: "していただく",
-            errors: [
-                {
-                    message: "rulePath test: \"頂く\" => \"いただく\"",
-                    line: 1,
-                    column: 3
-                }
-            ]
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"頂い\" => \"いただい\"",
+          line: 1,
+          column: 4
+        },
+        {
+          message: "ひらがなで表記したほうが読みやすい補助動詞: \"来よ\" => \"こよ\"",
+          line: 3,
+          column: 3
         }
-    ]
+      ]
+    }
+  ]
 
 });
